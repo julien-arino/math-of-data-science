aa9af496785e643e3527d7becebc2aa962bd6872
Julien Arino <julien.arino-UM@pm.me>
2026-09-02 17:23:50 +0200

    refactor: reorganize course slide deck structures, lecture materials, and figure assets

---
+++ CODE/L06-matrix-methods-QR-1.R
@@ -0,0 +1,18 @@
+## ----set-lecture-number,echo=FALSE--------------------------------------------
+lecture_number = "06"
+
+
+## ----set-options,echo=FALSE,warning=FALSE,message=FALSE-----------------------
+# Source the code common to all lectures
+source("common-code.R")
+
+
+## ----set-slide-background,echo=FALSE,results='asis'---------------------------
+# Are we plotting for a dark background? Setting is in common-code.R, but
+# cat command must run here.
+cat(input_setup)
+
+
+## ----convert-Rnw-to-R,warning=FALSE,message=FALSE,echo=FALSE,results='hide'----
+rmd_chunks_to_r_temp()
+
